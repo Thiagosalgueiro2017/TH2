ff765bdd372d248d85a770b1163858478054f863
ThiagoTrn2017 <tlima3767@gmail.com>
2017-08-02 11:44:42 -0300

    Projeto Completo

---
+++ oficina/Assets/scripts/MudarCena.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MudarCena : MonoBehaviour
+{
+
+    public void menu()
+    {
+        SceneManager.LoadScene("MarianaOUfavela");
+    }
+    public void Config()
+    {
+        SceneManager.LoadScene("Configuracao");
+    }
+    public void sair()
+    {
+        Application.Quit();
+    }
+    public void Favela()
+    {
+        SceneManager.LoadScene("Favela");
+    }
+    public void Mariana()
+     {
+       SceneManager.LoadScene("Mg1");
+     }
+}
